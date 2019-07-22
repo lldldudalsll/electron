89d1be3bae720e85ca55b98ea33c2f9b0ce4332e
youngmin <lldldudalsll@gmail.com>
2019-07-22 18:18:00 +0900

    angualr-electron project first commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatDatepickerModule,
+  MatDialogModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule,
+  MatProgressBarModule, MatProgressSpinnerModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSliderModule,
+  MatSlideToggleModule, MatSnackBarModule, MatTabsModule, MatToolbarModule, MatTooltipModule
+} from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatInputModule,
+    MatTabsModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatGridListModule,
+    MatCardModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatSliderModule,
+    MatAutocompleteModule,
+    MatDatepickerModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatNativeDateModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatRippleModule,
+    MatButtonToggleModule
+  ]
+})
+export class MaterialModule { }
